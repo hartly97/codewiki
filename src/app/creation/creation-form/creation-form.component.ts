f9c628c7b82bb1ae29687ba43fce91c880d3771b
Ian Brekelmans <ian.brekelmans@home.TheDomain>
2017-06-16 12:23:18 +0100

    added snippet model and comments route

--- src/app/creation/creation-form/creation-form.component.ts
+++ src/app/creation/creation-form/creation-form.component.ts
@@ -1,5 +1,7 @@
+import { Snippet, Language } from './../../shared/model/snippet';
 import { SnippetService } from './../../shared/services/snippet.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'cwiki-creation-form',
@@ -7,14 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creation-form.component.css']
 })
 export class CreationFormComponent implements OnInit {
+  snippet: Snippet;
 
   constructor(private snippetService: SnippetService) { }
 
   ngOnInit() {
+    this.snippet = new Snippet (
+      'Cool new snippet',
+      '*ngFor="let a of abc"',
+      'ianbrekelmans', 
+      [
+        new Language('angular',['2','4']),
+        new Language('html',['5']),
+        new Language('js',['5','6'])
+      ],
+      ['ng4','ng2','mehtod','for loop'],
+      'Beginner',
+      'Need to know'
+    );
   }
 
   onSave(){
-    this.snippetService.pushSnippet();
+    this.snippetService.pushSnippet(this.snippet);
 
   }
 
