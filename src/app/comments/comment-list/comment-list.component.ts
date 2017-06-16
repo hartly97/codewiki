064d93edf30094b004998b342f4281b0f7596586
susanna <studioswong@gmail.com>
2017-06-16 22:49:48 +0100

    starting comments module

---
+++ src/app/comments/comment-list/comment-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Comment, Mcomment} from '../../shared/model/comment';
+import {CommentCardComponent} from '../comment-card/comment-card.component';
+
+@Component({
+  selector: 'comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.css']
+})
+export class CommentListComponent implements OnInit {
+
+  @Input() comment;
+  overallComment = {main:{text:"jkhcjsklcndsjk",
+                       user:{id: "test",
+                             name: "hello gello",
+                             avatar: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+                             email: "testing@gmail.com"},
+                       upvotes: 0,
+                       date: "1606"},
+                    replies:[{text:"jkhcjsklcndsjk",
+                              user:{id: "test",
+                                    name: "hello gello2",
+                                    avatar: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+                                    email: "testing@gmail.com"},
+                              upvotes: 0,
+                              date: "1606"}, 
+                              {text:"jkhcjsklcndsjk",
+                                user:{id: "test",
+                                      name: "hello gello3",
+                                      avatar: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+                                      email: "testing@gmail.com"},
+                                upvotes: 0,
+                                date: "1606"}]
+                  };
+  replyComments;
+  mainComment;
+  needReplyBox;
+  
+  constructor() { }
+
+  ngOnInit() {
+    // split comments into main comment and sub-comment
+    this.splitComment(this.overallComment);
+
+    // load the comment 
+    console.log(this.replyComments);
+  }
+
+  splitComment(sComment: Mcomment){
+
+    this.mainComment = sComment.main;
+    this.replyComments = sComment.replies;
+  }
+
+  updateReply(){
+    this.needReplyBox = true;
+  }
+
+  onChildCommentSubmitted(event) {
+    this.replyComments.push(event);
+  }
+
+}
--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -1,8 +1,26 @@
+import { Comment } from './../model/comment';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class CommentService {
 
-  constructor() { }
+  comments: string = '/comments'
+  comments$: FirebaseListObservable<any>;
+
+  constructor(public db: AngularFireDatabase) { }
+
+
+  getComments():FirebaseListObservable<any>{
+    return this.db.list(this.comments);
+  }
+
+  pushComment(comment:Comment){
+    this.db.list(this.comments).push(comment);
+  }
+
+  ngOnInit(){
+    //this.comment$ = this.db.list('/comments');  
+  }
 
 }
